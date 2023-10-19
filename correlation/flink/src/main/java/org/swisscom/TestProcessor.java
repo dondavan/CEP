03f2ae08dc02565a6a264c2b5c94f18fa4795f58
dondavan <yeezushugh@gmail.com>
2023-10-19 13:44:35 +0200

    flink initial

---
+++ correlation/flink/src/main/java/org/swisscom/TestProcessor.java
@@ -0,0 +1,14 @@
+package org.swisscom;
+
+import org.apache.flink.streaming.api.functions.ProcessFunction;
+import org.apache.flink.util.Collector;
+
+/* Example process for event in datasource */
+public class TestProcessor extends ProcessFunction<String,String> {
+
+    @Override
+    public void processElement(String s, ProcessFunction<String, String>.Context context, Collector<String> collector) throws Exception {
+        System.out.println(s);
+    }
+
+}
